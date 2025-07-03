913b6ec56e646bef37c5bbbba239f7ae1743af8a
Jon Pass <jonathan@coassemble.com>
2025-07-03 10:27:22 +1000

    SCORM type

--- server/api/export.js
+++ server/api/export.js
@@ -1,5 +1,6 @@
 export default defineEventHandler(async (event) => {
    const config = useRuntimeConfig(event);
    const id = getQuery(event).id;
-   return await $fetch(`${config.url}/v1/headless/course/scorm/${id}`, { headers: config.headers });
+   const type = 'dynamic'; // static or dynamic
+   return await $fetch(`${config.url}/v1/headless/course/scorm/${id}?type=${type}`, { headers: config.headers });
  });
