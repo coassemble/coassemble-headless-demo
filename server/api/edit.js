7f262f0472d9346bdd88efe2cb534b6279d82726
Jon Pass <jonathan@coassemble.com>
2025-10-28 09:07:49 +1100

    Enable publishing

--- server/api/edit.js
+++ server/api/edit.js
@@ -15,6 +15,7 @@ export default defineEventHandler(async (event) => {
       color: '#C4BCFC', 
       translations: true,
       loom: true,
+      publishing: true,
       narrations: true
     }
   };
